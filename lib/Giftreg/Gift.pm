dfd9f2721de65b6ba6031f05f5968622682c8f9f
Joel Loudermilk <joel@loudermilk.org>
2012-01-17 16:56:07 -0500

    added unbuy/unbuy

---
+++ lib/Giftreg/Gift.pm
@@ -0,0 +1,154 @@
+package Giftreg::Gift;
+use Mojo::Base 'Mojolicious::Controller';
+use Giftreg::DB;
+
+sub add_helpers {
+  my $self = shift;
+  # Prints the un-buy button and link, or nothing, depending on who's logged in.
+  $self->app->helper(unbuy_button => sub {
+    my ($self, $gift) = @_;
+  
+    # Anonymous users never see the button.
+    return '' unless $self->user_exists;
+  
+    # Don't show the button if the gift is not bought.
+    return '' unless $gift->is_bought;
+  
+    # Show the button for gifts bought by the currently logged-in user,
+    # or for everything if this user owns the list.
+    if( $self->user->person_id == $gift->wanted_by_person_id ||
+        $self->user->person_id == $gift->bought_by_person_id ) {
+      my $link_url = $self->url_for('/gift/unbuy/' . $gift->gift_id);
+      return qq{<a href="$link_url" class="button">Unbuy</a>};
+    }
+  
+    return '';
+  });
+  
+  # Prints the edit button.
+  $self->app->helper(edit_button => sub {
+    my ($self, $gift) = @_;
+  
+    return '' unless $self->user_exists;
+  
+    if( $self->user->person_id == $gift->wanted_by_person_id ) {
+      my $link_url = $self->url_for('/gift/edit/' . $gift->gift_id);
+      return qq{<a href="$link_url" class="button">Edit</a>};
+    }
+  
+    return '';
+  });
+  
+  # Prints the delete button.
+  $self->app->helper(delete_button => sub {
+    my ($self, $gift) = @_;
+  
+    return '' unless $self->user_exists;
+  
+    if( $self->user->person_id == $gift->wanted_by_person_id ) {
+      my $link_url = $self->url_for('/gift/delete/' . $gift->gift_id);
+      return qq{<a href="$link_url" class="button">Delete</a>};
+    }
+  
+    return '';
+  });
+
+  # Prints the buy button.
+  $self->app->helper(buy_button => sub {
+    my ($self, $gift) = @_;
+  
+    return '' if $gift->is_bought;
+  
+    my $link_url = $self->url_for('/gift/buy/' . $gift->gift_id);
+    my $link_text=qq{<a href="$link_url" class="button">Buy</a>};
+  
+    if( !$self->user_exists ) {
+      return $link_text;
+    }
+  
+    if( $self->user->person_id == $gift->wanted_by_person_id ) {
+      return '';
+    }
+  
+    return $link_text;
+  });
+  
+  # Prints the bought message, for bought gifts the user can't unbuy.
+  $self->app->helper(bought_message => sub {
+    my ($self, $gift) = @_;
+  
+    if( $gift->is_bought && $self->unbuy_button($gift) eq '' ) {
+      return 'Already bought';
+    }
+  
+    return '';
+  });
+}
+
+sub unbuy {
+  my $self = shift;
+
+  $self->require_login() or return;;
+
+  # when we're done, we'll redirect to the previous URL
+  my $prev_url = $self->req->headers->referrer || '/';
+
+  my $gift_id = $self->param('gift_id');
+  my $db = Giftreg::DB->connect();
+  my $gift = $db->resultset('Gift')->find($gift_id);
+
+  if( !defined $gift ) {
+    $self->flash('error_message' => 'The gift you selected is unknown.');
+    $self->redirect_to($prev_url);
+    return;
+  }
+
+  $prev_url = '/user/view/' . $gift->wanted_by_person_id;
+
+  if( $gift->wanted_by_person_id != $self->user->person_id &&
+      $gift->bought_by_person_id != $self->user->person_id ) {
+    $self->flash('error_message' => 'You can only unbuy gifts you bought ' .
+                 'or gifts on your list.');
+    $self->redirect_to($prev_url);
+    return;
+  }
+
+  $gift->bought_by_person_id(undef);
+  $gift->update;
+  $self->flash('message' => 'The gift you selected has been unbought.');
+  $self->redirect_to($prev_url);
+}
+
+sub buy {
+  my $self = shift;
+
+  $self->require_login() or return;
+
+  # After this action, we redirect to the previous URL.
+  my $prev_url = $self->req->headers->referrer || '/';
+
+  my $gift_id = $self->param('gift_id');
+  my $db = Giftreg::DB->connect();
+  my $gift = $db->resultset('Gift')->find($gift_id);
+
+  if( !defined $gift ) {
+    $self->flash('error_message' => 'The gift you selected is unknown.');
+    $self->redirect_to($prev_url);
+    return;
+  }
+
+  $prev_url = '/user/view/' . $gift->wanted_by_person_id;
+
+  if( $gift->is_bought ) {
+    $self->flash('error_message' => 'The gift you selected is already bought.');
+    $self->redirect_to($prev_url);
+    return;
+  }
+
+  $gift->bought_by_person_id($self->user->person_id);
+  $gift->update;
+  $self->flash('message' => 'The gift you selected has been bought.');
+  $self->redirect_to($prev_url);
+}
+
+1;
---
+++ t/gift.t
@@ -0,0 +1,62 @@
+#!/usr/bin/env perl
+use Mojo::Base -strict;
+
+use Test::More tests => 27;
+use Test::Mojo;
+use DBI;
+
+$ENV{MOJO_MODE} = 'testing';
+
+use_ok 'Giftreg';
+use_ok 'Giftreg::DB';
+
+my $t = Test::Mojo->new('Giftreg');
+
+# populate the database
+my $db = Giftreg::DB->connect();
+$db->deploy();
+$db->populate('Person', [
+  [ qw/ person_id email_address password last_update_dt / ],
+  [ 1, 'person1@example.com', 'person1', 1322006400 ],
+  [ 2, 'person2@example.com', 'person2', undef ],
+  [ 3, 'person3@example.com', 'person3', undef ],
+]);
+$db->populate('Gift', [
+  [ qw/ gift_id short_desc long_desc location
+        wanted_by_person_id bought_by_person_id priority_nbr / ],
+  [ 1, 'Gift 1', 'Longer description of gift 1', 'Anywhere', 1, undef, 3 ],
+  [ 2, 'Gift 2', 'Longer description of gift 2', 'Anywhere', 1, 3, 1 ],
+]);
+
+# Log in and buy a gift
+$t->max_redirects(1);
+$t->post_form_ok('/login', {
+  username => 'person2@example.com',
+  password => 'person2',
+});
+$t->get_ok('/gift/buy/3')->status_is(200)
+  ->content_like(qr/The gift you selected is unknown/i, 'buy invalid gift');
+$t->get_ok('/gift/buy/2')->status_is(200)
+  ->content_like(qr/The gift you selected is already bought/i,
+                 'buy already-bought gift');
+$t->get_ok('/gift/buy/1')->status_is(200)
+  ->content_like(qr/The gift you selected has been bought/i,
+                 'buy available gift');
+my $gift = $db->resultset('Gift')->find(1);
+ok($gift->is_bought, 'gift 1 is bought in db');
+
+# Unbuy the gift just bought
+$t->get_ok('/user/view/1')->status_is(200)
+  ->content_like(qr/Unbuy/, 'unbuy button present');
+$t->get_ok('/gift/unbuy/3')->status_is(200)
+  ->content_like(qr/The gift you selected is unknown/i, 'unbuy invalid gift');
+$t->get_ok('/gift/unbuy/2')->status_is(200)
+  ->content_like(qr/You can only unbuy gifts you bought/i,
+                 q{unbuy someone else's gift});
+$gift = $db->resultset('Gift')->find(2);
+ok($gift->is_bought, 'gift 2 is still bought in db');
+$t->get_ok('/gift/unbuy/1')->status_is(200)
+  ->content_like(qr/The gift you selected has been unbought/i,
+                 'unbuy eligible gift');
+$gift = $db->resultset('Gift')->find(1);
+ok(!$gift->is_bought, 'gift 1 is not bought in db');
